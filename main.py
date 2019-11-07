67ffe571a53e99d6f6ec37c0f47b88dd4672cd68
Jesper Petersen <info@ordklar.dk>
2019-11-07 09:14:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+import threading
+import time
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for index in range(5):
+        logging.info("Main    : before creating thread %d", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        logging.info("Main    : before running thread")
+        x.start()
+
+    for index, thread in enumerate(thread):
+        logging.info("Main    : wait for the thread to finish %d", index)
+        x.join()
+        logging.info("Main    : all %d done", index)
